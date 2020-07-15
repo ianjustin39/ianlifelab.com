0c63c28a6010f3dfb6cd893c70e7126c37634524
ina.tsai1 <ian.tsai@coolbitx.com>
2020-07-15 10:51:43 +0800

    toc teat1

--- js/toc.js
+++ js/toc.js
@@ -18,7 +18,9 @@ $.smoothScroll({
   autoCoefficent: 2
 });
 
+// var url = "./jquery.smooth - scroll.min.js";
 var url = "https://cdnjs.cloudflare.com/ajax/libs/jquery-smooth-scroll/2.2.0/jquery.smooth-scroll.min.js";
+
 // Bind the hashchange event listener
 $.getScript( url, function() {
   $(window).bind('hashchange', function (event) {
